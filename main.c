ee47753c5dd6dc4a23a868dc00aeda91c19ae8a4
j1sk1ss <fotikolay222@gmail.com>
2025-04-19 10:18:22 +0000

    New main.c logic

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        print_error("Input files required!\nFor usage use -h.");
-        return -1;
+        print_error("Input files required!");
+        print_info("For usage use -h.");
+        return EXIT_FAILURE;
     }
     
     mm_init();
@@ -15,24 +16,21 @@ int main(int argc, char* argv[]) {
     char* output_location = NULL;
     for (int i = 1; i < argc; i++) {
         if (!str_strcmp(argv[i], "-h")) {
-            print_info("Usage:");
-            print_info("Print syntax tree: --syntax");
-            print_info("Output location: -o");
-            print_info("Show this message: -h");
+            printf("|===============================\n"         );
+            printf("|| Usage:\n"                                );
+            printf("|| Print syntax tree: --syntax\n"           );
+            printf("|| Output location: -o\n"                   );
+            printf("|| Show this message: -h\n"                 );
+            printf("|| Save output generated asm code: --asm\n" );
+            printf("|===============================\n"         );
         }
-        else if (!str_strcmp(argv[i], "--syntax")) {
-            p.syntax = 1;
-        }
-        else if (!str_strcmp(argv[i], "--asm")) {
-            p.save_asm = 1;
-        }
-        else if (!str_strcmp(argv[i], "-o")) {
-            output_location = argv[++i];
-        }
+        else if (!str_strcmp(argv[i], "--syntax")) p.syntax = 1;
+        else if (!str_strcmp(argv[i], "--asm")) p.save_asm = 1;
+        else if (!str_strcmp(argv[i], "-o")) output_location = argv[++i];
         else {
             if (!output_location) {
                 print_error("Output location not provided!");
-                return 0;
+                return EXIT_FAILURE;
             }
 
             build(argv[i], first);
@@ -44,7 +42,8 @@ int main(int argc, char* argv[]) {
     int build_res = build_all(output_location);
     if (!build_res) {
         print_error("Error via compilation! Code: %i", build_res);
+        return EXIT_FAILURE;
     }
 
-    return 1;
+    return EXIT_SUCCESS;
 }
