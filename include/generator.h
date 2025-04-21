b2cd7ea5d26a1a485cc4e54394bb857a55715aa4
j1sk1ss <fotikolay222@gmail.com>
2025-04-21 16:18:46 +0000

    Generator cleanup

--- include/generator.h
+++ include/generator.h
@@ -4,7 +4,7 @@
 #include "vars.h"
 #include "syntax.h"
 
-#define ARRAYS_MAX_TOKEN    100
+#define ARRAYS_MAX_TOKEN 100
 #define iprintf(out, fmt, ...) fprintf(out, "%*s" fmt, _current_depth * 4, "", ##__VA_ARGS__)
 #define GET_ASMVAR(node) \
     (!((node)->token->ro || (node)->token->glob) ? \
@@ -23,10 +23,6 @@ static inline char* format_from_data(unsigned char* name) {
     return buf;
 }
 
-static inline char* formal_variable(tree_t* node, tree_t* name_node) {
-    static char buf[128] = { 0 };
-    return NULL;
-}
 
 /*
 */
